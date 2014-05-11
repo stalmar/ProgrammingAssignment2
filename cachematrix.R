748c3007ddac41c83b7fc27beb6d3a9b5c9e0b3c
stalmar <marcin.stalij@gmail.com>
2014-05-11 12:13:59 +0200

    caching assignment for R course

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Function which transcribes plain matrix into list that contains matrix and cache of inversion
 
-makeCacheMatrix <- function(x = matrix()) {
+
+makeMatrix <- function(x = matrix()) { 
+  i <- NULL                           #sets variable for inversion
+  set <- function(y) {                #records given matrix
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x                 # function to get the matrix
+  setinverse <- function(inverse) i <<- inverse # function to cache inversion
+  getinverse <- function() i          # function to get the caches inversion
+  list(set = set, get = get           # gives a list with acces to original matrix, caches inversion and functions to write and read them
+       
+       setinverse = setinverse,
+       getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+##  Function which computes inversion of a given matrix or reads from cache if already computed
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheInverse <- function(x, ...) {
+  
+  i <- x$getinverse()             # checks the cache
+  if(!is.null(i)) {               # if inversion is already there
+    message("getting cached inversion")   
+    return(i)                     #then it is just read and returned
+  }
+  temp <- x$get()                 # if inversion is not cached, original matrix is recalled
+  i <- solve(temp,...)            # inversion is computed
+  x$setinverse(i)                 # and put into cache
+  i                               # finally, either way, inversion is returned
+  
 }
+
+
